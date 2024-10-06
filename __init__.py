ca7098c4ca40e63f908a71333fa6b8f1651acc75
ALOKDUBEYFORU <DubeyAlok@rediffmail.com>
2024-10-06 11:48:57 +0530

    Updated withdraw function in multiprocessing lock

--- __init__.py
+++ __init__.py
@@ -9,8 +9,12 @@ def deposit(balance,lock):
         balance.value = balance.value+1
         lock.release()
 
-def withdraw(balance):
-    pass
+def withdraw(balance,lock):
+    for i in range(100):
+        time.sleep(.01)
+        lock.acquire()
+        balance.value = balance.value - 1
+        lock.release()
 
 if __name__ == '__main__':
     balance = multiprocessing.Value('i',2000)
