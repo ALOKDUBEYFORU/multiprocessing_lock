d7dca5cdcfafae82b1dce7d5e90b00c5a982ca72
ALOKDUBEYFORU <DubeyAlok@rediffmail.com>
2024-10-06 05:49:49 +0530

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import time
+import multiprocessing
+
+def deposit(balance):
+    pass
+
+def withdraw(balance):
+    pass
+
+if __name__ == '__main__':
+    balance = multiprocessing.Value('i',2000)
+    pass
